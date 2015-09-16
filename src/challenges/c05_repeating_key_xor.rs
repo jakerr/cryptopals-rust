0e8099165dc1c636332f361a716585f21b895097
Jake Kerr <kodafox@gmail.com>
2015-09-16 22:28:43 +0900

    Fix dead code warnings by putting main behind cfg
    Fix unused value warning by unwrapping the write! results.

--- src/challenges/c05_repeating_key_xor.rs
+++ src/challenges/c05_repeating_key_xor.rs
@@ -1,10 +1,11 @@
 extern crate cryptopals;
 
-use std::env;
-use std::io::{stdin, stdout, Read, Write};
-use cryptopals::conversions::hex_to_string;
-
+#[cfg(not(test))]
 fn main() {
+    use std::env;
+    use std::io::{stdin, stdout, Read, Write};
+    use cryptopals::conversions::hex_to_string;
+
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
         panic!("Requires one argument which is the key with which to do repeating key xor \
@@ -15,7 +16,7 @@ fn main() {
     let mut stdout = stdout();
     for (a, b) in stdin.zip(key) {
         if a == 10 { continue } // Line feed
-        write!(&mut stdout, "{}", hex_to_string(&[a ^ b]));
+        write!(&mut stdout, "{}", hex_to_string(&[a ^ b])).unwrap();
     }
-    write!(&mut stdout, "\n");
+    write!(&mut stdout, "\n").unwrap();
 }
